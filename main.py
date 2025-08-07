7f9da74d58c5ea36f44b48440f7ff3fb44ebfa90
Kang-dongbae <skylove7890@gmail.com>
2025-08-07 16:30:23 +0900

    add

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-#from src.data_processing import load_data, preprocess_data
+from src.data_processing import load_data, preprocess_data
 #from src.feature_engineering import extract_features
 #from src.model.train import train_model
 #from src.model.predict import predict
 #from src.optimization.maintenance import optimize_maintenance
-from src.utils.logger import setup_logging
+#from src.utils.logger import setup_logging
 
 def main():
     # Test logging setup
-    logger = setup_logging()
+    #logger = setup_logging()
     #logger.debug("Debug message")
     #logger.info("Info message")
     #logger.warning("Warning message")
     #logger.error("Error message")
     
     # 데이터 로드 및 전처리
-#    raw_data = load_data("data/raw/sensor_data.csv")
+    raw_data = load_data(r"C:\Dev\KorailTM\test.csv")
+    print(raw_data.head())
 #    processed_data = preprocess_data(raw_data)
 #    features = extract_features(processed_data)
     
---
+++ src/data_processing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_data(file):
+    data = pd.read_csv(file)
+    return data
+
+def preprocess_data(data):
+
+    return data
