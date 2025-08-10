d6ff0d5f550e7c3fe94baa367988f2c811346a2b
Kang-dongbae <skylove7890@gmail.com>
2025-08-10 11:46:51 +0900

    modify

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from src.data_processing import load_data, preprocess_data
-#from src.feature_engineering import extract_features
-#from src.model.train import train_model
-#from src.model.predict import predict
-#from src.optimization.maintenance import optimize_maintenance
+import yaml
+#from src.data.data_processing import load_and_preprocess
+#from src.data.feature_eng import extract_features
+#from src.utils.evaluation import evaluate
+
+# 모델 모드별 import
+#from src.models import supervised, unsupervised, transfer
 
 def main():
+    
+    # 1. 설정 불러오기
+    with open('config.yaml', 'r', encoding='utf-8') as f:
+        cfg = yaml.safe_load(f)
+    
+    print(cfg)
     # 데이터 로드
-    raw_data = load_data(r"C:\Dev\KorailTM\test.csv")
-    print(raw_data.head())
-    print("test")
+    #raw_data = load_and_preprocess(r"C:\Dev\KorailTM\test.csv")
+    #print(raw_data.head())
+    #print("test")
 
     # 데이터 전처리
 #    processed_data = preprocess_data(raw_data)
